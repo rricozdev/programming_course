148f2fe4df3fb8336f8a8caaeb6e10c02094ee0e
Ricardo Rico <programador5784@gmail.com>
2025-02-20 10:32:25 -0500

    update App.jsx
    Actualizando componentes para el uso de rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import LandingPage from './pages/LandingPage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import RegistrationForm from "./pages/RegistrationForm";
 
 function App() {
-
   return (
-    <>
-      <LandingPage />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/registro" element={<RegistrationForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
