ede6efc58ef87054c706072198dab9cbe3dbf4c0
Ricardo Rico <programador5784@gmail.com>
2025-02-21 11:46:21 -0500

    Update App.jsx
    Creando comodin en las rutas, si no es "/" o "/register" va a Error404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import RegistrationForm from "./pages/RegistrationForm";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/registro" element={<RegistrationForm />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
   );
