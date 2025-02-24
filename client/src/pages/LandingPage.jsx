82ff2eaac10e5fc3e667fc9963aec48116d38e67
Ricardo Rico <programador5784@gmail.com>
2025-02-24 09:43:13 -0500

    Update LandingPage.jsx
    Actualizando tamaño del logo

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -422,7 +422,7 @@ const LandingPage = () => {
         <div className="max-w-7xl mx-auto px-4">
           <div className="flex justify-between items-center h-16">
             <div className="flex-shrink-0">
-              <img src={logo} alt="Logo" className="h-16 w-52" />
+              <img src={logo} alt="Logo" className="h-16 w-2xs" />
             </div>
 
             {/* Desktop Menu */}
